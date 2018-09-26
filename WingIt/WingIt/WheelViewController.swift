87cf2d608cb8ed578a1c97024f427bef26c0d3af
Jonah Zukosky <jonah.zukosky@gmail.com>
2018-09-25 19:08:22 -0500

    Added Base Controller

---
+++ WingIt/WingIt/WheelViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WheelViewController.swift
+//  WingIt
+//
+//  Created by Jonah Zukosky on 9/25/18.
+//  Copyright © 2018 Zukosky, Jonah. All rights reserved.
+//
+
+import UIKit
+
+class WheelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
